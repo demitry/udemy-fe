4eb6634228164576e32beab5c9b67fede80adb26
Dmytro Poluektov <dmytro.poluektov@everymatrix.com>
2017-06-15 15:37:31 +0300

    hero section

--- vue/compare-vue/src/components/Home.vue
+++ vue/compare-vue/src/components/Home.vue
@@ -1,16 +1,50 @@
 <template>
   <div class="home">
-    Home, Sweet Home
+    <!-- Hero Section -->
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <!-- use interpolation (heading from export defaults) -->
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-4">{{ subheading }}</h2>
+          </div>
+          <a class="button is-large is-primary" id="learn">Learn More</a>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      heading: 'Soaring to new heights',
+      subheading: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang='sass' scoped>
 @import '../mq'
+
+.hero
+  background: url('../assets/clouds.jpg')
+  background-size: cover
+
+  .title
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+
+  h2
+    margin: 1.5rem 0 2rem 0 !important
 </style>
