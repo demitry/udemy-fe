40a8f545acca79f97d92215c53a1ce3848a472bd
DESKTOP-FCMOFM8\Dmitry <aurorabitbucket@gmail.com>
2017-06-17 13:29:47 +0300

    footer

--- react/compare-react/src/components/Footer/Footer.js
+++ react/compare-react/src/components/Footer/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
-
+import './Footer.css'
 const Footer = () => {
   return (
     <div className="App">
-      MY FOOTER
+      <footer className="footer is-primary">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <p>Footer: where you can put stuff</p>
+            </div>
+            <div className="column has-text-right">
+              <a className="icon" href="#">
+                <i className="fa fa-facebook"></i>
+              </a>
+              <a className="icon" href="#">
+                <i className="fa fa-twitter"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
